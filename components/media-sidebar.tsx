d0d29351d679b1c0d4006883aa696317f53557b8
sina sinat <sinathgb@gmail.com>
2025-06-15 14:18:43 +0700

    update media-sidebar

--- components/media-sidebar.tsx
+++ components/media-sidebar.tsx
@@ -1,15 +1,40 @@
-import Image from "next/image";
-
+import Link from "next/link";
+import { Github, Dribbble ,Figma} from "lucide-react";
 export default function MediaSidebar() {
   return (
     <div className="fixed left-4 top-20 transform -translate-y-1/2 z-10 hidden lg:block">
-      <Image
-        src="/images/media.svg"
-        alt="Social media links"
-        width={32}
-        height={311}
-        className="opacity-80 hover:opacity-100 transition-opacity"
-      />
+      <div className="flex flex-col items-center space-y-4">
+        {/* Vertical line */}
+        <div
+          className="w-px h-48 opacity-40 transition-colors duration-300"
+          style={{ backgroundColor: "var(--foreground)" }}
+        ></div>
+
+        {/* Social media icons */}
+        <div className="flex flex-col space-y-4">
+          <Link
+            href="https://github.com/SSINAT"
+            className="transition-colors duration-300"
+            style={{ color: "var(--foreground)" }}
+          >
+            <Github className="h-5 w-5 hover:text-primary" />
+          </Link>
+          <Link
+            href="#"
+            className="transition-colors duration-300"
+            style={{ color: "var(--foreground)" }}
+          >
+            <Dribbble className="h-5 w-5 hover:text-primary" />
+          </Link>
+          <Link
+            href="#"
+            className="transition-colors duration-300"
+            style={{ color: "var(--foreground)" }}
+          >
+            <Figma className="h-5 w-5 hover:text-primary" />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
